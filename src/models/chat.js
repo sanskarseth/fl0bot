ff0d000629e96e8ecc593a0c77e76c847fb092dc
sanskarseth <sethsanskar@gmail.com>
2023-06-12 10:18:19 +0530

    added uuid:

--- src/models/chat.js
+++ src/models/chat.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const { Sequelize, DataTypes } = require('sequelize');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = (sequelize) => {
   const Chat = sequelize.define(
     'Chat',
     {
       chat_id: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.UUID,
         primaryKey: true,
         autoIncrement: true,
       },
@@ -23,18 +24,26 @@ module.exports = (sequelize) => {
         type: DataTypes.STRING,
       },
       content: {
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(500)
       },
       time_created: {
         type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW, 
       },
       time_updated: {
         type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW, 
       },
     },
     {
       tableName: 'chats', // Specify the table name explicitly if different from the model name
       timestamps: false, // Disable timestamps (createdAt, updatedAt)
+      hooks: {
+        beforeValidate: (chat, options) => {
+          // Update the time_updated field to the current timestamp before saving the record
+          chat.time_updated = new Date();
+        },
+      },
     }
   );
 
