d473216343f4d337473ddf2d56b44962022a5b01
sanskarseth <sethsanskar@gmail.com>
2023-06-13 00:35:11 +0530

    added slack

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ app.post('/slack/action-endpoint', async (req, res) => {
       try {
         switch(req.body.event.type) {
           case 'app_mention':
-            const response = await handleAppMention(req.body)
+            const response = handleAppMention(req.body)
             res.status(200).json({ message: 'Success' });
             break
           default:
@@ -71,7 +71,7 @@ async function handleAppMention({event}) {
       { person_id, role: 'user', content: query },
       { person_id, role: 'assistant', content: response.data.choices[0].message.content }
     ]);
-    // await axios.post(process.env[config.slack_webhook], {text: response.data.choices[0].message.content});
+    await axios.post(process.env[config.slack_webhook], {text: response.data.choices[0].message.content});
     return response.data.choices[0].message.content
   } catch (error) {
     console.log("ERROR",error)
