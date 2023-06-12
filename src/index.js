ba10f3c637def2fcad8c7ac97a82f57f264b09dc
sanskarseth <sethsanskar@gmail.com>
2023-06-13 00:31:50 +0530

    added slack

--- src/index.js
+++ src/index.js
@@ -71,7 +71,7 @@ async function handleAppMention({event}) {
       { person_id, role: 'user', content: query },
       { person_id, role: 'assistant', content: response.data.choices[0].message.content }
     ]);
-    axios.post(process.env[config.slack_webhook], {text: response.data.choices[0].message.content});
+    // await axios.post(process.env[config.slack_webhook], {text: response.data.choices[0].message.content});
     return response.data.choices[0].message.content
   } catch (error) {
     console.log("ERROR",error)
