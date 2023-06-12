e50a8c4b0220b8757d56c8daccc50034fe53819e
sanskarseth <sethsanskar@gmail.com>
2023-06-12 09:59:18 +0530

    updated app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const { sequelize, Chat, Person } = require('./models');
 const e = require('express');
-app.use(express.json());
+
 const app = express()
+app.use(express.json());
+
 const port = process.env.PORT ?? 3000;
 
 app.get('/', (req, res) => {
