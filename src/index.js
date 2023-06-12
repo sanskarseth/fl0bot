6c32a735687751a47aced346e1f6b16c047f817c
sanskarseth <sethsanskar@gmail.com>
2023-06-13 00:00:52 +0530

    added slack

--- src/index.js
+++ src/index.js
@@ -31,13 +31,14 @@ app.post('/slack/action-endpoint', async (req, res) => {
             const response = await handleAppMention(req.body)
             res.status(200).json({ message: response });
             break
-          default: 
+          default:
+            res.status(400).json({ message: 'Bad Request' });
             break
         }
       } catch (error) {
         console.error(`Error processing Slack event: ${error}`);
+        res.status(500).json({ message: error });
       }
-    res.status(200).json({ message: 'Event received' });
   }
 });
 
