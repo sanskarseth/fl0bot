3b9976ba14accbd860d604b6852804c3819cb474
Sanskar Seth <55178412+sanskarseth@users.noreply.github.com>
2023-06-14 23:36:19 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -6,6 +6,13 @@ module.exports = {
     "slack_webhook" : "SLACK_WEBHOOK",
     synchronize: true
   },
+  "development": {
+    "use_env_variable": "DATABASE_URL",
+    "openai_api_key": "OPENAI_API_KEY",
+    "bot_system" : "BOT_SYSTEM",
+    "slack_webhook" : "SLACK_WEBHOOK",
+    synchronize: true
+  },
   "production": {
     "use_env_variable": "DATABASE_URL",
     "openai_api_key": "OPENAI_API_KEY",
